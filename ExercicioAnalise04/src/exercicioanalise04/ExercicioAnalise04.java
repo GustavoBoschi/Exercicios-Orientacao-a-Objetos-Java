45cbf68d2f0317442d4b1d44db759327b808b766
Gustavo Boschini <127453464+GustavoBoschi@users.noreply.github.com>
2023-04-07 21:48:15 -0300

    Add files via upload

---
+++ ExercicioAnalise04/src/exercicioanalise04/ExercicioAnalise04.java
@@ -0,0 +1,13 @@
+package exercicioanalise04;
+
+public class ExercicioAnalise04 {
+
+    public static void main(String[] args) {
+        
+        ExibirMes qMes = new ExibirMes();
+        
+        qMes.mes();
+        
+    }
+    
+}
