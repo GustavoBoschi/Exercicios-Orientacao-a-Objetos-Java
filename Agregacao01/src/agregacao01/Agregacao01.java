26aea63f2176ed9f49bcf983ea1fa80aa886d532
Gustavo Boschini <127453464+GustavoBoschi@users.noreply.github.com>
2023-05-18 11:26:57 -0300

    Add files via upload

---
+++ Agregacao01/src/agregacao01/Agregacao01.java
@@ -0,0 +1,13 @@
+package agregacao01;
+
+public class Agregacao01 {
+
+    public static void main(String[] args) {
+        
+        Lutador lutador = new Lutador();
+        
+        lutador.apresentar();
+        lutador.ganharLuta();
+        lutador.apresentar();
+    }    
+}
